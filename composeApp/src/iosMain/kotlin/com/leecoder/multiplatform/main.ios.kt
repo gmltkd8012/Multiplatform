91282b134ef65b485b3e7ca3821b7dcdcd34a706
LeeCoder <heesangdev@gmail.com>
2025-12-18 22:30:52 +0900

    [LeeCoder] Feat: 누락 코드 추가

---
+++ composeApp/src/iosMain/kotlin/com/leecoder/multiplatform/main.ios.kt
@@ -0,0 +1,11 @@
+package com.leecoder.multiplatform
+
+import androidx.compose.ui.uikit.OnFocusBehavior
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() =
+    ComposeUIViewController(
+        configure = {
+            onFocusBehavior = OnFocusBehavior.DoNothing
+        }
+    ) { App() }
